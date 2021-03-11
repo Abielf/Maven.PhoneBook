b6c965aab1fccacfaa027cf1681921d8c127bdea
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-03-10 22:59:41 -0500

    lab done but the tests are NOT playing nice with my getAllContactNames method

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,39 +13,83 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook =map;
     }
 
     public PhoneBook() {
-        this(null);
+        phonebook=new HashMap<String, List<String>>(); {
+        };
     }
 
+
     public void add(String name, String phoneNumber) {
+        List<String> tempList = phonebook.get(name);
+        if(tempList == null) {
+            tempList = new ArrayList<>();
+            phonebook.put(name, tempList);
+        }
+        tempList.add(phoneNumber);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        List<String> tempList = phonebook.get(name);
+        List<String> numList= Arrays.asList(phoneNumbers);
+        if(tempList == null) {
+            tempList = new ArrayList<>();
+            phonebook.put(name, tempList);
+        }
+        for(String i:phoneNumbers){tempList.add(i);}
+
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        List<String> tempList = phonebook.get(name);
+        if (tempList==null){
+            return false;
+        }return true;
+    }
+
+    public Boolean hasEntry(String name, String number) {
+        List<String> tempList = phonebook.get(name);
+        if (tempList==null){
+            return false;
+        }return true;
     }
 
     public List<String> lookup(String name) {
+
+        if(hasEntry(name)){
+            return phonebook.get(name);
+        }
         return null;
     }
 
     public String reverseLookup(String phoneNumber)  {
+        List<String> nameList=getAllContactNames();
+        List<String> temp=getAllContactNames();
+        for (String i:nameList){
+            temp=phonebook.get(i);
+            for(String j:temp){ if(j==phoneNumber){ return i;}}
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String>nameList=new ArrayList<String>();
+        for (String i: phonebook.keySet()){
+            nameList.add(i);
+        }
+        Collections.sort(nameList);
+        Collections.reverse(nameList);
+        return nameList;
+
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return phonebook;
     }
 }
